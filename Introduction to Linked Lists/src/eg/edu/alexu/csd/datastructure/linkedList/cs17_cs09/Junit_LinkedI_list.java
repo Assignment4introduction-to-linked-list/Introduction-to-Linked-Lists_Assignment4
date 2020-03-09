10113de08755c38946bb524c1abb0e4fa3d16049
ahmed <ahmed@192.168.1.7>
2020-03-09 18:57:19 +0200

    Adding new junit test cases

---
+++ Introduction to Linked Lists/src/eg/edu/alexu/csd/datastructure/linkedList/cs17_cs09/Junit_LinkedI_list.java
@@ -0,0 +1,14 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs17_cs09;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Junit_LinkedI_list {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
