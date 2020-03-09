16f510ae1d1e875df9b9ba923599c8efa1117fc0
ahmed <ahmed@192.168.1.7>
2020-03-09 18:00:55 +0200

    initial folders

---
+++ Introduction to Linked Lists/src/eg/edu/alexu/csd/datastructure/linkedList/cs17_cs09/Linked_list.java
@@ -0,0 +1,65 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs17_cs09;
+
+public class Linked_list implements ILinkedList {
+
+	@Override
+	public void add(int index, Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void set(int index, Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void remove(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ILinkedList sublist(int fromIndex, int toIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Introduction to Linked Lists/src/eg/edu/alexu/csd/datastructure/linkedList/cs17_cs09/OurMain.java
@@ -0,0 +1,14 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs17_cs09;
+
+public class OurMain {
+
+    public static void main(String[] args) {
+        System.out.println("Let's count from 1 to 10");
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(i); 
+        }
+
+    }
+
+}
