d3e86aa22e9fd7476494fd5e77b5b846aea49a0c
ahmed <ahmed@192.168.1.7>
2020-03-16 01:58:01 +0200

    UI application with a problem in set method

---
+++ Introduction to Linked Lists/src/eg/edu/alexu/csd/datastructure/linkedList/csX22/UI_Application.java
@@ -0,0 +1,84 @@
+package eg.edu.alexu.csd.datastructure.linkedList.csX22;
+
+import java.util.Scanner;
+
+public class UI_Application {
+	public static void main (String [] args) {
+		try (Scanner s = new Scanner(System.in)) {
+			PolynomialSolver p =new PolynomialSolver();
+			int choose;
+			int test=0;
+			char Vname1,Vname2;
+			while (true) {
+				for (int i=0;i<20;i++)System.out.println();
+				if (test!=0) {
+					System.out.println("Please Insert A Correct Number");
+				}
+				test=0;
+				System.out.println("Please Choose An Action\n------------------------\n1- Set a polynomial variable\n"+
+				"2- Print the value of a polynomial variable\n3- Add two polynomials\n4- Subtract two polynomials\n5- Multiply two polynomials\r\n" + 
+				"6- Evaluate a polynomial at some point\n7- Clear a polynomial variable\n");
+				choose=s.nextInt();
+				switch(choose) {
+				case 1:{
+					System.out.println("Insert the variable name: A, B or C");
+					Vname1=s.next().charAt(0);
+					int [] CoEx =new int[2]; 
+					System.out.println("Insert the polynomial terms in the form:\n(coeff1, exponent1), (coeff2, exponent2), .. ");
+					Single_Linked_list l=new Single_Linked_list();
+					while(s.hasNextInt()) {
+						CoEx[0]=s.nextInt();CoEx[1]=s.nextInt();
+						l.add(CoEx);
+					}
+					Object[] terms=new Object[l.size()];
+					terms=l.listToArr(l);
+					p.setPolynomial(Vname1, (int[][]) terms);
+					System.out.println("Polynomial "+Vname1+"is set");break;
+					}
+				case 2:{
+					System.out.println("Insert the variable name: A, B, C or R ");
+					Vname1 = s.next().charAt(0);
+					System.out.println("Value in"+Vname1+":"+p.print(Vname1));break;
+					}
+				case 3 :{System.out.println("Insert the first variable name: A, B, C or R ");
+					Vname1=s.next().charAt(0);
+					System.out.println("Insert the Second variable name: A, B, C or R ");
+					Vname2=s.next().charAt(0);
+					p.setPolynomial('R',p.add(Vname1, Vname2));
+					System.out.println("the result of adding :"+p.print('R'));break;
+					}
+				case 4 :{
+					System.out.println("Insert the first variable name: A, B, C or R ");
+					Vname1=s.next().charAt(0);
+					System.out.println("Insert the Second variable name: A, B, C or R ");
+					Vname2=s.next().charAt(0);
+					p.setPolynomial('R',p.subtract(Vname1, Vname2));
+					System.out.println("the result of Subtracting :"+p.print('R'));break;
+					}
+				case 5 :{
+					System.out.println("Insert the first variable name: A, B, C or R ");
+					Vname1=s.next().charAt(0);
+					System.out.println("Insert the Second variable name: A, B, C or R ");
+					Vname2=s.next().charAt(0);
+					p.setPolynomial('R',p.multiply(Vname1, Vname2));
+					System.out.println("the result of Multiplying :"+p.print('R'));break;
+					}
+				case 6:{
+					System.out.println("Insert the variable name: A, B, C or R ");
+					Vname1=s.next().charAt(0);
+					System.out.println("Enter the Value you want to Evaluate Polynomial at it:");
+					float x = s.nextFloat();
+			        System.out.println("Value of "+Vname1+" when x = "+ x + "is"+p.evaluatePolynomial(Vname1,x));break;
+					}
+				case 7:{
+					System.out.println("Insert the variable name: A, B, C or R ");
+					Vname1=s.next().charAt(0);
+					p.clearPolynomial(Vname1);
+					System.out.println("Done");break;
+					}
+				default : 	test=1;
+				}
+			}
+		}
+	}
+}
--- Introduction to Linked Lists/src/eg/edu/alexu/csd/datastructure/linkedList/csX22/UI_class.java
+++
@@ -1,5 +0,0 @@
-package eg.edu.alexu.csd.datastructure.linkedList.csX22;
-
-public class UI_class {
-
-}
